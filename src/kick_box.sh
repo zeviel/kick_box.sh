b6846efdf667c98809fbfdf78216fa62983af679
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 19:39:56 +0800

    Update kick_box.sh

--- src/kick_box.sh
+++ src/kick_box.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 api="https://open.kickbox.com/v1"
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 
 function check_domain() {
 	curl --request GET \
 		--url "$api/disposable/$1" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "accept: application/json"	\
 		--header "content-type: application/json"
 }
